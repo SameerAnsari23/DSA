ab672c8f385f0f5aa4d0dd9247fc0e33ea4f6b48
Sameer Ansari <117716456+SameerAnsari23@users.noreply.github.com>
2025-10-07 18:28:14 +0530

    Create user_input.cpp

---
+++ String/User_Input/user_input.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main() {
+ string s;
+ // cin>>s;
+ // cin.getline(s);
+ // getline(cin, s);
+ // cout << s;
+
+ // int n;
+ // cin >> n;
+ // cin.get();
+ // for(int i = 0; i < n; i++) {
+ //     getline(cin, s);
+ //     // cout << s << endl;
+ // }
+ // for(int i = 0; i < n; i++) {
+ //     // getline(cin, s);
+ //     cout << s << endl;
+ // }
+ 
+ string x = "apple bananan mango";
+ 
+ cout << x.substr(7, 4);
+
+}
