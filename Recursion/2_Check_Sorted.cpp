25149675d80c828ae666c97031e9f0e0e5b29a3e
Sameer Ansari <117716456+SameerAnsari23@users.noreply.github.com>
2025-12-06 14:56:55 +0530

    Implement isSorted function for array sorting check
    Fix recursion logic to check if an array is sorted.

--- Recursion/2_Check_Sorted.cpp
+++ Recursion/2_Check_Sorted.cpp
@@ -1,1 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+bool isSorted(vector<int>& arr, int i) {
+ //base case
+ if (i == arr.size()-1) return true;
+ if (arr[i] > arr[i+1]) return false;
+ isSorted(arr, i+1);
+}
+
+int main() {
+ vector<int>arr = {7, 8, 9, 4, 15};
+ if (isSorted(arr, 0)) {
+  cout << "Sorted" << endl;
+ }
+ else {
+  cout << "Not Sorted" << endl;
+ }
+}
