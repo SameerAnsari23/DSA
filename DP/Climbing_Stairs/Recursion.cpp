e653a57247fb431e601ec9c6ea12962d68902243
Sameer Ansari <117716456+SameerAnsari23@users.noreply.github.com>
2026-01-23 11:16:59 +0530

    Add recursive solution for climbing stairs problem

---
+++ DP/Climbing_Stairs/Recursion.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int solve(int n) {
+        if (n == 1 || n == 2)
+            return n;
+
+        return solve(n - 1) + solve(n - 2);
+    }
+
+    int climbStairs(int n) {
+        return solve(n);
+    }
+};
