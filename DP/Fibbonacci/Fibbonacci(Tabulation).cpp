0c4bc407e3764e53e4e325fbb22951689922a408
Sameer Ansari <117716456+SameerAnsari23@users.noreply.github.com>
2026-01-23 11:13:22 +0530

    Refactor Fibonacci to use tabulation method

--- DP/Fibbonacci/Fibbonacci(Tabulation).cpp
+++ DP/Fibbonacci/Fibbonacci(Tabulation).cpp
@@ -1,27 +1,19 @@
 #include <iostream>
 using namespace std;
 
-int dp[1000];
-
-int fibMemo(int n) {
-    if (n <= 1)
-        return n;
-
-    if (dp[n] != -1)
-        return dp[n];
-
-    return dp[n] = fibMemo(n - 1) + fibMemo(n - 2);
-}
-
 int main() {
     int n;
     cin >> n;
 
-    for (int i = 0; i <= n; i++)
-        dp[i] = -1;
+    int dp[n];
+    dp[0] = 0;
+    if (n > 1) dp[1] = 1;
+
+    for (int i = 2; i < n; i++)
+        dp[i] = dp[i - 1] + dp[i - 2];
 
     for (int i = 0; i < n; i++)
-        cout << fibMemo(i) << " ";
+        cout << dp[i] << " ";
 
     return 0;
 }
