b64c59f4e1f091e6ab8af57f141076a0c914bba0
Sameer Ansari <117716456+SameerAnsari23@users.noreply.github.com>
2026-01-23 11:14:50 +0530

    Add optimized Fibonacci calculation in C++

---
+++ DP/Fibbonacci/Factorial(Optimization).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int a = 0, b = 1;
+    cout << a << " ";
+
+    for (int i = 1; i < n; i++) {
+        cout << b << " ";
+        int c = a + b;
+        a = b;
+        b = c;
+    }
+
+    return 0;
+}
